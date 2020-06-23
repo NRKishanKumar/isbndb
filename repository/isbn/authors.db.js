4fa4c2ece79f2ab3ed660226ff06d0e549c7d59c
Kumar <kishan.93k@gmail.com>
2020-06-23 12:38:20 +0530

    Added query for authors

--- repository/isbn/authors.db.js
+++ repository/isbn/authors.db.js
@@ -39,7 +39,25 @@ function getRetailInventory(params, next) {
     const collection = db.get().collection('books');
     return new Promise(async (resolve, reason) => {
         try {
-            collection.find({})
+            await collection.aggregate([{
+                $lookup:
+                    {
+                        from: "authors",
+                        localField: "author",
+                        foreignField: "name",
+                        as: "owner"
+                    }
+            }, {"$unwind": "$owner"}, {
+                "$group": {
+                    _id: "$_id",
+                    "TotalAmount": {
+                        "$sum": "$price"
+                    },
+                    "noOfBooks": {
+                        "$sum": "$sold"
+                    }
+                }
+            }])
                 .toArray((err, result) => {
                     resolve(result);
                     next && next(null, result);
